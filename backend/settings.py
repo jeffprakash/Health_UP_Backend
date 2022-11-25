056e2d1b4e59073396f95c5fd1eec54dac13f463
jeffprakash <jeffprakash@gmail.com>
2022-11-25 17:55:47 +0530

    new

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api.apps.ApiConfig",
-     "rest_framework",
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,8 +51,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",    #note
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True    #note this
 ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
